1a996d674d3dedf8e41fe5a331eba96185e2313c
Moyaccercchi <moyaccercchi@hotmail.de>
2015-12-09 23:02:03 +0000

    [FEATURE] split GML files apart
    for easier handling

---
+++ javascript/GML_UI.js
@@ -0,0 +1,661 @@
+/*
+	*****************************************************
+		GML UI - Graph Merging Library User Interface
+	*****************************************************
+*/
+
+window.GML_UI = {
+
+	init: function() {
+
+		GML.set_to_HTML();
+
+		// every on mouse up should release the verbosity capture
+		document.addEventListener('mouseup', function() {
+			GML_UI.changeOptions_verbosity_capture = false;
+		}, false);
+
+		// default to default options ;)
+		this.resetOptions();
+
+		// default to tab 5
+		this.showTab(5);
+	},
+
+
+
+	// stores the visibility of div-out so that we don't reset it when changing tabs
+	div_out_visibility: [false, false, false, false, false, false, false],
+
+	// how many tabs are there?
+	upToTabs: 7,
+
+
+
+	/*
+		Tab Control
+	*/
+
+	unShowAllTabs: function() {
+		for (var i = 0; i < this.upToTabs; i++) {
+			document.getElementById('tab-btn-' + i).className = 'tabbutton';
+			document.getElementById('div-in-' + i).style.display = 'none';
+			document.getElementById('div-out-' + i).style.display = 'none';
+			var el = document.getElementById('div-xbw-' + i);
+			if (el) {
+				el.style.display = 'none';
+			}
+		}
+	},
+
+	showTab: function(nexttab) {
+		this.unShowAllTabs();
+
+		document.getElementById('tab-btn-' + nexttab).className = 'tabbutton active';
+		document.getElementById('div-in-' + nexttab).style.display = 'block';
+		if (this.div_out_visibility[nexttab]) {
+			document.getElementById('div-out-' + nexttab).style.display = 'block';
+			if (!GML.hideXBWenvironments) {
+				var el = document.getElementById('div-xbw-' + nexttab);
+				if (el) {
+					el.style.display = 'block';
+				}
+			}
+		}
+	},
+
+	setJumpDispStyle: function(i, show) {
+
+		var dispstyle = 'none';
+
+		if (show) {
+			dispstyle = 'inline';
+		}
+
+		var j = 1;
+		var el = document.getElementById('a-jump-' + i + '-' + j);
+
+		while (el) {
+			el.style.display = dispstyle;
+			el = document.getElementById('a-jump-' + i + '-' + j);
+			j++;
+		}
+	},
+
+	activateDivOut: function(i, showXBWenv, showAnchors) {
+
+		var el = document.getElementById('div-xbw-' + i);
+		if (el) {
+			if (showXBWenv && !GML.hideXBWenvironments) {
+				el.style.display = 'block';
+			} else {
+				el.style.display = 'none';
+			}
+		}
+
+
+		this.setJumpDispStyle(i, showAnchors);
+
+
+		el = document.getElementById('div-out-' + i);
+		el.innerHTML = '<div>... working on your request ...</div>';
+		el.style.display = 'block';
+
+		this.div_out_visibility[i] = true;
+
+		return el;
+	},
+
+
+
+	/*
+		Tab 0 - Generate One BWT (naively)
+	*/
+
+	generateNaiveBWT: function() {
+		var el = this.activateDivOut(0, false, true);
+		el.innerHTML = '<div>' + GML.generate_BWT_naively(
+			document.getElementById('in-string-0').value.toUpperCase()) + '</div>';
+	},
+
+
+
+
+	/*
+		Tab 1 - Merge Two BWTs (naively)
+	*/
+
+	generateNaiveBWTs: function() {
+		var el = this.activateDivOut(1, false, false);
+		el.innerHTML = '<div>' + GML.generate_BWTs_naively(
+			document.getElementById('in-string-1-1').value.toUpperCase(),
+			document.getElementById('in-string-1-2').value.toUpperCase()) + '</div>';
+	},
+
+	mergeNaiveBWTs: function() {
+		var el = this.activateDivOut(1, false, true);
+		el.innerHTML = '<div>' + GML.merge_BWTs_naively(
+			document.getElementById('in-string-1-1').value.toUpperCase(),
+			document.getElementById('in-string-1-2').value.toUpperCase()) + '</div>';
+	},
+
+
+
+	/*
+		Tab 2 - Generate One BWT (advanced)
+	*/
+	generateAdvancedBWT: function() {
+		var el = this.activateDivOut(2, true, true);
+		el.innerHTML = '<div>' + GML.generate_BWT_advanced(
+			document.getElementById('in-string-2').value.toUpperCase()) + '</div>';
+	},
+
+
+
+
+	/*
+		Tab 3 - Merge Two BWTs (advanced)
+	*/
+
+	generateAdvancedBWTs: function() {
+		var el = this.activateDivOut(3, false, true);
+		el.innerHTML = '<div>' + GML.generate_BWTs_advanced(
+			document.getElementById('in-string-3-1').value.toUpperCase(),
+			document.getElementById('in-string-3-2').value.toUpperCase()) + '</div>';
+	},
+
+	mergeAdvancedBWTs: function() {
+		var el = this.activateDivOut(3, true, true);
+		el.innerHTML = '<div>' + GML.merge_BWTs_advanced(
+			document.getElementById('in-string-3-1').value.toUpperCase(),
+			document.getElementById('in-string-3-2').value.toUpperCase()) + '</div>';
+	},
+
+
+
+	/*
+		Tab 5 - Merge Two XBWs
+	*/
+
+	mergeGraphXBWs: function() {
+		var el = this.activateDivOut(5, true, true);
+		el.innerHTML = '<div>' + GML.merge_XBWs(
+			document.getElementById('in-string-5-1').value.toUpperCase(),
+			document.getElementById('in-string-5-2').value.toUpperCase()) + '</div>';
+	},
+
+
+
+	/*
+		Tab 6 - Fuse Two XBWs
+	*/
+
+	fuseGraphXBWs: function() {
+		var el = this.activateDivOut(6, true, true);
+
+		// TODO EMRG :: actually implement fuse_XBWs within GML
+		el.innerHTML = '<div><div class="error">Sorry, this has not yet been implemented.</div></div>';
+		return;
+
+		el.innerHTML = '<div>' + GML.fuse_XBWs(
+			document.getElementById('in-string-6-1').value.toUpperCase(),
+			document.getElementById('in-string-6-2').value.toUpperCase()) + '</div>';
+	},
+
+
+
+	/*
+		Info fields
+	*/
+
+	s_naiveInputFormat:
+		'Input format:<br>' +
+		'<ul>' +
+		'<li>in general, all characters are just entered as plain text string, e.g. "ACA"</li>' +
+		'<li>lower case characters will automatically be converted to upper case, ' +
+		'e.g. "aca" to "ACA"</li>' +
+		'<li>to encode a graph, use the bubble notation, ' +
+		'e.g. to encode both "AAA" and "ACA", use "A(A|C)A"</li>' +
+		'<li>do not add a dollar sign at the end of the input, as it will be added automagically</li>' +
+		'</ul>' +
+		'</div>',
+
+	s_advancedInputFormat:
+		'Input format:<br>' +
+		'<ul>' +
+		'<li>in general, all characters of the main path (from # to $) ' +
+		'are just entered as plain text string, e.g. "ACA"</li>' +
+		'<li>lower case characters will automatically be converted to upper case, ' +
+		'e.g. "aca" to "ACA"</li>' +
+		'<li>to encode a graph, add a single pipe character after the main path, ' +
+		'followed by infoblocks for each path, separated by semicolons, ' +
+		'e.g. "mainpath|infoblock;infoblock;infoblock</li>' +
+		'<li>each infoblock contains exactly four parts, separated by commas, ' +
+		'e.g. "ACA|1,2,3,4;1,2,3,4;1,2,3,4"' +
+		'<ol>' +
+		'<li>the first part is the identifier of the path (it can be empty)</li>' +
+		'<li>the second part is the origin of the path, containing the identifier of the path on which this one originates followed by ":" and the position in that path on which it originates (counting starts at 1 for the first alphabetical character, as the hash tag symbol in the main path is symbol 0) - the identifier of the main path is "0", but in the special case of the main path the identifer and the ":" can be left out together, e.g. "8" for the eighth position on the main path or "path9:8" for the eighth position in a path with the identifer "path9"</li>' +
+		'<li>the third part is the content of the path (it can be empty), e.g. "TGC"</li>' +
+		'<li>the fourth part is the target of the path, containing the identifier of the path on which this one ends followed by ":" and the position in that path on which it ends (counting starts at 1 for the first alphabetical character, as the hash tag symbol in the main path is symbol 0) - the identifier of the main path is "0", but in the special case of the main path the identifer and the ":" can be left out together, e.g. "8" for the eighth position on the main path or "path9:8" for the eighth position in a path with the identifer "path9"</li>' +
+		'</ol>' +
+		'</li>' +
+		'<li>overall, a valid graph can look like "GACG|p1,1,TGG,3;,p1:0,C,p1:2" - this ' +
+		'example could in bubble notation be rewritten as G(A|T(G|C)G)CG</li>' +
+		'<li>do neither add a hash tag symbol at the start ' +
+		'nor a dollar sign at the end of the input, as they will be added automagically</li>' +
+		'</ul>' +
+		'</div>',
+
+	s_input:
+		'Use this field to specify the graph ' + GML.DH +
+		' for which the BWT will be generated.<br><br>',
+
+	s_input1:
+		'Use this field to specify the first graph, ' + GML.DH_1 +
+		', which will be merged with ' + GML.DH_2 + '.<br><br>',
+
+	s_input2:
+		'Use this field to specify the second graph, ' + GML.DH_2 +
+		', which will be merged with ' + GML.DH_1 + '.<br><br>',
+
+	generateNaiveBWTIn1Info: function(e) {
+		var el = this.activateDivOut(0, false, false);
+		
+		var sout = '<div>';
+		sout += '<u>Na&iuml;ve Graph BWT Generation - Input</u><br><br>';
+
+		sout += this.s_input;
+		sout += this.s_naiveInputFormat;
+
+		el.innerHTML = sout;
+		e.stopPropagation();
+	},
+
+	mergeNaiveBWTsIn1Info: function(e) {
+		var el = this.activateDivOut(1, false, false);
+		
+		var sout = '<div>';
+		sout += '<u>Na&iuml;ve Graph BWT Merging - Input 1</u><br><br>';
+
+		sout += this.s_input1;
+		sout += this.s_naiveInputFormat;
+
+		el.innerHTML = sout;
+		e.stopPropagation();
+	},
+
+	mergeNaiveBWTsIn2Info: function(e) {
+		var el = this.activateDivOut(1, false, false);
+		
+		var sout = '<div>';
+		sout += '<u>Na&iuml;ve Graph BWT Merging - Input 2</u><br><br>';
+
+		sout += this.s_input2;
+		sout += this.s_naiveInputFormat;
+
+		el.innerHTML = sout;
+		e.stopPropagation();
+	},
+
+	mergeNaiveBWTsInfo: function(e) {
+		var el = this.activateDivOut(1, false, false);
+		
+		var sout = '<div>';
+		sout += '<u>Na&iuml;ve Graph BWT Merging</u><br><br>';
+		sout += 'Limitations:<br>';
+		sout += '<ul>';
+		sout += '<li>only works with at most one bubble in ' + GML.H_1 + ' and ' + GML.H_2 +
+				' each (with each bubble having two alternatives, each being one character long)</li>';
+		sout += '<li>only sorts ' + GML.H_1 + ' and ' + GML.H_2 +
+				' by first bubble alternative (so if ' + GML.H_1 + ' is sorted before ' +
+				GML.H_2 + ', then ' + GML.H_1 + ' gets ' + GML.DS_1_o +
+				', even if the alternative path in ' + GML.H_1 + ' would be sorted after ' +
+				GML.H_2 + ' - it would be better to give ' + GML.DS_1_o +
+				' up to $<span class="d">4</span> to both alternatives of both strings separately, ' +
+				'instead of assigning the same $ to each alternative in the string)</li>';
+		sout += '</ul>';
+		sout += '</div>';
+
+		el.innerHTML = sout;
+
+		e.stopPropagation();
+	},
+
+
+	generateAdvancedBWTIn1Info: function(e) {
+		var el = this.activateDivOut(2, false, false);
+		
+		var sout = '<div>';
+		sout += '<u>Graph BWT Generation - Input</u><br><br>';
+
+		sout += this.s_input;
+		sout += this.s_advancedInputFormat;
+
+		el.innerHTML = sout;
+		e.stopPropagation();
+	},
+
+
+	mergeAdvancedBWTsIn1Info: function(e) {
+		var el = this.activateDivOut(3, false, false);
+		
+		var sout = '<div>';
+		sout += '<u>Graph BWT Merging - Input 1</u><br><br>';
+
+		sout += this.s_input1;
+		sout += this.s_advancedInputFormat;
+
+		el.innerHTML = sout;
+		e.stopPropagation();
+	},
+
+	mergeAdvancedBWTsIn2Info: function(e) {
+		var el = this.activateDivOut(3, false, false);
+		
+		var sout = '<div>';
+		sout += '<u>Graph BWT Merging - Input 2</u><br><br>';
+
+		sout += this.s_input2;
+		sout += this.s_advancedInputFormat;
+
+		el.innerHTML = sout;
+		e.stopPropagation();
+	},
+
+
+	mergeGraphXBWsIn1Info: function(e) {
+		var el = this.activateDivOut(5, false, false);
+		
+		var sout = '<div>';
+		sout += '<u>Graph XBW Merging - Input 1</u><br><br>';
+
+		sout += this.s_input1;
+		sout += this.s_advancedInputFormat;
+
+		el.innerHTML = sout;
+		e.stopPropagation();
+	},
+
+	mergeGraphXBWsIn2Info: function(e) {
+		var el = this.activateDivOut(5, false, false);
+		
+		var sout = '<div>';
+		sout += '<u>Graph XBW Merging - Input 2</u><br><br>';
+
+		sout += this.s_input2;
+		sout += this.s_advancedInputFormat;
+
+		el.innerHTML = sout;
+		e.stopPropagation();
+	},
+
+
+	fuseGraphXBWsIn1Info: function(e) {
+		var el = this.activateDivOut(6, false, false);
+		
+		var sout = '<div>';
+		sout += '<u>Graph XBW Fusing - Input 1</u><br><br>';
+
+		sout += this.s_input1;
+		sout += this.s_advancedInputFormat;
+
+		el.innerHTML = sout;
+		e.stopPropagation();
+	},
+
+	fuseGraphXBWsIn2Info: function(e) {
+		var el = this.activateDivOut(6, false, false);
+		
+		var sout = '<div>';
+		sout += '<u>Graph XBW Fusing - Input 2</u><br><br>';
+
+		sout += this.s_input2;
+		sout += this.s_advancedInputFormat;
+
+		el.innerHTML = sout;
+		e.stopPropagation();
+	},
+
+
+	hideObject: function(whichOne) {
+		
+		var svg_el = document.getElementById('hide-cont-' + whichOne);
+		var svg_hide_el = document.getElementById('hide-btn-' + whichOne);
+		svg_el = svg_el.childNodes[1];
+
+		if (svg_el.style.display == 'none') {
+			svg_el.style.display = 'block';
+			svg_hide_el.innerHTML = 'Hide';
+		} else {
+			svg_el.style.display = 'none';
+			svg_hide_el.innerHTML = 'Show';
+		}
+	},
+
+
+
+	changeOptions_verbosity_capture: false,
+	changeOptions_verbosity_compwidth: 100,
+
+	changeOptions_verbosity_mouse: function(e, down) {
+		this.changeOptions_verbosity_capture = down;
+
+		if (down) {
+			this.changeOptions_verbosity_move(e);
+		}
+	},
+
+	changeOptions_verbosity_move: function(e) {
+		if (this.changeOptions_verbosity_capture) {
+			var rect = document.getElementById('in-options-verbosity').getBoundingClientRect();
+			var compwidth = ((100 * (e.clientX - rect.x)) / rect.width);
+
+			this.changeOptions_verbosity_compwidth = compwidth;
+
+			this.changeOptions_verbosity_update();
+		}
+	},
+
+	changeOptions_verbosity_to_str: [
+		'tell me nothing', // 0
+		'the results and nothing else please', // 1
+		'low', // 2
+		'below medium', // 3
+		'slightly below medium', // 4
+		'medium', // 5
+		'slightly above medium', // 6
+		'above medium', // 7
+		'high', // 8
+		'very high', // 9
+		'tell me everything', // 10
+	],
+
+	changeOptions_verbosity_update: function() {
+
+			if (this.changeOptions_verbosity_compwidth > 99) {
+				this.changeOptions_verbosity_compwidth = 100;
+			}
+			if (this.changeOptions_verbosity_compwidth < 1) {
+				this.changeOptions_verbosity_compwidth = 0;
+			}
+
+			document.getElementById('in-options-verbosity-inner').style.width =
+				this.changeOptions_verbosity_compwidth + '%';
+
+			var verbosity = Math.round((this.changeOptions_verbosity_compwidth + 5) / 10);
+			if (verbosity < 1) {
+				verbosity = 1;
+			}
+			if (verbosity > 10) {
+				verbosity = 10;
+			}
+
+			document.getElementById('verbosity-out').innerHTML =
+				this.changeOptions_verbosity_to_str[verbosity];
+
+			GML.verbosity = verbosity;
+
+			this.animateApplyBtn(true);
+	},
+
+	changeOptions_show_xbw_envs: function() {
+
+		var el = document.getElementById('in-options-show-xbw-envs');
+
+		if (el.innerHTML == 'X') {
+			el.innerHTML = '&nbsp;';
+		} else {
+			el.innerHTML = 'X';
+		}
+
+		this.animateApplyBtn(true);
+	},
+
+	changeOptions_show_graph: function() {
+
+		var el = document.getElementById('in-options-show-graph');
+		var it = document.getElementById('show-graph-info-text');
+
+		if (el.innerHTML == 'X') {
+			el.innerHTML = '&nbsp;';
+			it.style.display = 'none';
+		} else {
+			el.innerHTML = 'X';
+			it.style.display = 'inline';
+		}
+
+		this.animateApplyBtn(true);
+	},
+
+	changeOptions_show_autoi: function() {
+
+		var el = document.getElementById('in-options-show-autoi');
+
+		if (el.innerHTML == 'X') {
+			el.innerHTML = '&nbsp;';
+		} else {
+			el.innerHTML = 'X';
+		}
+
+		this.animateApplyBtn(true);
+	},
+
+	applyBtncurrentlyanimated: false,
+	applyBtncurrentlyintervalID: 0,
+	applyBtncurrentlycallint: 0,
+	applyBtncurrentlygoingdown: true,
+
+	// if startanimation is set to false, then we stop the animation instead
+	animateApplyBtn: function(startanimation) {
+
+		if (this.applyBtncurrentlyanimated !== startanimation) {
+
+			this.applyBtncurrentlyanimated = startanimation;
+
+			this.applyBtncurrentlycallint = 0;
+
+			if (startanimation) {
+
+				this.applyBtncurrentlyintervalID = window.setInterval(
+					this.animateApplyBtnCall, 50);
+
+			} else {
+
+				clearInterval(this.applyBtncurrentlyintervalID);
+				this.animateApplyBtnCall();
+			}
+		}
+	},
+
+	animateApplyBtnCall: function() {
+
+		if (this.applyBtncurrentlygoingdown) {
+			this.applyBtncurrentlycallint -= 5;
+		} else {
+			this.applyBtncurrentlycallint += 5;
+		}
+
+		if (this.applyBtncurrentlycallint < -17) {
+			this.applyBtncurrentlygoingdown = false;
+			this.applyBtncurrentlycallint = -17;
+		}
+
+		if (this.applyBtncurrentlycallint > 108) {
+			this.applyBtncurrentlygoingdown = true;
+			this.applyBtncurrentlycallint = 108;
+		}
+
+		var c = 238 - this.applyBtncurrentlycallint;
+		document.getElementById('id-apply-btn').style.backgroundColor =
+			'rgb(' + c + ',' + c + ',' + c + ')';
+	},
+
+	applyOptions: function() {
+
+		GML.origin_1 = document.getElementById('in-options-index-1').value;
+		GML.origin_2 = document.getElementById('in-options-index-2').value;
+
+		GML.ao = parseInt(document.getElementById('in-options-array-offset').value, 10);
+
+		this.saveOptions();
+	},
+
+	resetOptions: function() {
+
+		document.getElementById('in-options-index-1').value = '0';
+		document.getElementById('in-options-index-2').value = '1';
+
+		document.getElementById('in-options-array-offset').value = '0';
+
+		document.getElementById('in-options-show-xbw-envs').innerHTML = 'X';
+		document.getElementById('in-options-show-graph').innerHTML = '&nbsp;';
+		document.getElementById('in-options-show-autoi').innerHTML = '&nbsp;';
+
+		this.changeOptions_verbosity_compwidth = 100;
+
+		this.applyOptions();
+	},
+
+	saveOptions: function() {
+
+		for (var i = 0; i < this.upToTabs; i++) {
+			this.div_out_visibility[i] = false;
+			this.setJumpDispStyle(i, false);
+		}
+
+		GML.hideXBWenvironments = document.getElementById('in-options-show-xbw-envs').innerHTML != 'X';
+
+		GML.set_to_HTML();
+
+		var dispstyle = 'none';
+
+		if (document.getElementById('in-options-show-graph').innerHTML == 'X') {
+			dispstyle = 'inline-block';
+		}
+
+		GML.show_auto_i = document.getElementById('in-options-show-autoi').innerHTML == 'X';
+
+		document.getElementById('tab-btn-0').style.display = dispstyle;
+		document.getElementById('tab-btn-1').style.display = dispstyle;
+
+		var env_links = document.getElementsByClassName('xbw-env-link');
+		var env_disp = 'block';
+
+		if (GML.hideXBWenvironments) {
+			env_disp = 'none';
+		}
+
+		for (var i=0; i < env_links.length; i++) {
+			env_links[i].style.display = env_disp;
+		}
+
+		this.changeOptions_verbosity_update();
+
+		this.animateApplyBtn(false);
+	},
+
+};
+
+
+
+GML_UI.init();
